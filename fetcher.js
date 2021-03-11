8d08a8312431623ee5d7f9a58509167fb6eea73c
code-driving <tittan7@gmail.com>
2021-03-11 04:59:44 +0000

    fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -3,48 +3,43 @@
 const request = require("request");
 const fs = require("fs");
 const readline = require("readline");
-const { on } = require("node:events");
+// const { on } = require("node:events");
 const args = process.argv.slice(2);
 const URL = args[0];
 const localFilePath = args[1];
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 // console.log('URL===>', URL)
 // console.log('localFilePath===>', localFilePath)
 
-request
-  .get(URL)
-  .on("error", (err) => {
-    console.log(err);
-  })
-  .on("response", (response) => {
-    // fs.lstatSync(localFilePath).isDirectory();
-    fs.writeFile(
-      localFilePath,
-      response,
-      {
-        encoding: "utf8",
-      },
-      (err) => {
-        if (err) {
-          //if THE GIVEN PATH IS INVALID
-          if (err.code == "ENOENT") {
-            //let the user know about this issue
-          }
-          if (response.statusCode !== 200) {
-            //terminate the app explaining to the user what went wrong, and not write the response body to the file.
-            rl.write("Sorry, the connection was interrupted.");
-            rl.close();
-          }
-          // if (/*file already exists */) {
-          //     //overwrite the file without prompting ||
-          //     //let the user know and prompt them to type in Y(followed by the enter key) to overwrite the file, otherwise skip and exit the app.
-          //     //USE HERE READLINE
-          // }
+//function to get the size of the received data
+const getDataSize = (data) => {
+  return fs.statSync(data).size;
+};
+
+const getResponse = (url) => {
+  request(url, (error, response, receivedData) => {
+    // if (error) throw error;
+
+    try {
+      fs.writeFile(localFilePath, receivedData, { encoding: "utf8" }, (error) => {
+        if (error) throw error;
+        if (response.statusCode !== 200) {
+          console.log("Sorry. Connection was interrupted");
         }
-      }
-    );
+      });
+      console.log(
+        `Downloaded and saved ${getDataSize(
+          receivedData
+        )} bytes to ${localFilePath}`
+      );
+    } catch (error) {
+      console.log(error)
+    }
   });
+};
+
+getResponse(URL);
