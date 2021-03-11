3ea480d9152b22e1f4a117225c1146596ecfb975
code-driving <tittan7@gmail.com>
2021-03-11 04:16:08 +0000

    working on the fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,37 +1,50 @@
 /*Implement a small command line node app called fetcher.js which should take a URL as a command-line argument as well as a local file path and download the resource to the specified path.*/
 
-const request = require('request');
-const fs = require('fs')
-const readline = require('readline');
-const args = process.argv.slice(2)
-const URL = args[0]
-const localFilePath = args[1]
+const request = require("request");
+const fs = require("fs");
+const readline = require("readline");
+const { on } = require("node:events");
+const args = process.argv.slice(2);
+const URL = args[0];
+const localFilePath = args[1];
 
-console.log('URL===>', URL)
-console.log('localFilePath===>', localFilePath)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+// console.log('URL===>', URL)
+// console.log('localFilePath===>', localFilePath)
 
-// const req = request('', (error, res, body) => {
-  
-//   res.on('data', (d) => {
-    
-//   })
-// })
-
-// request('http://www.example.edu', (error, response, body) => {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// rl.question('What do you think of Node.js? ', (answer) => {
-//   // TODO: Log the answer in a database
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
-
-//   rl.close();
-// });
+request
+  .get(URL)
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("response", (response) => {
+    // fs.lstatSync(localFilePath).isDirectory();
+    fs.writeFile(
+      localFilePath,
+      response,
+      {
+        encoding: "utf8",
+      },
+      (err) => {
+        if (err) {
+          //if THE GIVEN PATH IS INVALID
+          if (err.code == "ENOENT") {
+            //let the user know about this issue
+          }
+          if (response.statusCode !== 200) {
+            //terminate the app explaining to the user what went wrong, and not write the response body to the file.
+            rl.write("Sorry, the connection was interrupted.");
+            rl.close();
+          }
+          // if (/*file already exists */) {
+          //     //overwrite the file without prompting ||
+          //     //let the user know and prompt them to type in Y(followed by the enter key) to overwrite the file, otherwise skip and exit the app.
+          //     //USE HERE READLINE
+          // }
+        }
+      }
+    );
+  });
